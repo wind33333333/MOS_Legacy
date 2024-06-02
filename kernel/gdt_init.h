a96d98eeaa960ee8415f7a5c46cef189bdfee67e
wind33333333 <wind33333333@gmail.com>
2024-06-02 20:39:44 +0800

    20240525

--- kernel/gdt_init.h
+++ kernel/gdt_init.h
@@ -1,13 +1,15 @@
 #ifndef __GDT_INIT_H__
 #define __GDT_INIT_H__
+#include "lib.h"
+#include "memory.h"
 
 void    gdt_init(unsigned int bsp_flags);
 
 struct gdt{
     unsigned short limit;
-    unsigned long base;
+    unsigned long *base;
 } __attribute__((packed));
 
-struct gdt gdt_ptr={0,0};
+__attribute__((section(".init_data"))) struct gdt gdt_ptr={0,0};
 
 #endif
