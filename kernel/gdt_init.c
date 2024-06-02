090ae06d61fb673f5fb1a8ed230e94b5d89d0911
wind33333333 <wind33333333@gmail.com>
2024-06-02 20:51:19 +0800

    20240525

--- kernel/gdt_init.c
+++ kernel/gdt_init.c
@@ -2,13 +2,12 @@
 
 __attribute__((section(".init_text"))) void gdt_init(unsigned int bsp_flags) {
     if (bsp_flags) {
-        gdt_ptr.limit = ((cpu_num * 16 + 0x50) + 0xFFF) & PAGE_4K_MASK - 1;
-        gdt_ptr.limit = 0x3fff;
+        gdt_ptr.limit = ((cpu_num << 4 + 0x50) + 0xFFF) & PAGE_4K_MASK - 1;
 
         for(unsigned int i = (gdt_ptr.limit+1) >> PAGE_4K_SHIFT; i != 0; i--){
             gdt_ptr.base = alloc_pages();
         }
-        memory_management_struct.kernel_end = gdt_ptr.base;
+        memory_management_struct.kernel_end = Phy_To_Virt(gdt_ptr.base);
         gdt_ptr.base -= (gdt_ptr.limit & PAGE_4K_MASK)/sizeof(long);
 
 
