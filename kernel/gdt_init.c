e6a3e8cc1a6b47220d518ae0f12edd167af784c2
wind33333333 <wind33333333@gmail.com>
2024-06-03 11:05:16 +0800

    20240525

--- kernel/gdt_init.c
+++ kernel/gdt_init.c
@@ -10,12 +10,12 @@ __attribute__((section(".init_text"))) void gdt_init(unsigned int bsp_flags) {
         gdt_ptr.base -= (gdt_ptr.limit & PAGE_4K_MASK)/sizeof(long);     //创建gdt指针结构
 
         *(gdt_ptr.base+0) = 0;        /*0	NULL descriptor		       	00*/
-        *(gdt_ptr.base+1) = 0;        /*1	KERNEL	Code	64-bit	Segment	08*/
-        *(gdt_ptr.base+2) = 0;        /*2	KERNEL	Data	64-bit	Segment	10*/
-        *(gdt_ptr.base+3) = 0;        /*3	USER	Code	64-bit	Segment	18*/
-        *(gdt_ptr.base+4) = 0;        /*4	USER	Data	64-bit	Segment	20*/
-        *(gdt_ptr.base+5) = 0;        /*5	KERNEL	Code	32-bit	Segment	28*/
-        *(gdt_ptr.base+6) = 0;        /*6	KERNEL	Data	32-bit	Segment	30*/
+        *(gdt_ptr.base+1) = CODE64_0;        /*1	KERNEL	Code	64-bit	Segment	08*/
+        *(gdt_ptr.base+2) = DATA64_0;        /*2	KERNEL	Data	64-bit	Segment	10*/
+        *(gdt_ptr.base+3) = CODE64_3;        /*3	USER	Code	64-bit	Segment	18*/
+        *(gdt_ptr.base+4) = DATA64_3;        /*4	USER	Data	64-bit	Segment	20*/
+        *(gdt_ptr.base+5) = CODE32_0;        /*5	KERNEL	Code	32-bit	Segment	28*/
+        *(gdt_ptr.base+6) = DATA32_0;        /*6	KERNEL	Data	32-bit	Segment	30*/
         *(gdt_ptr.base+7) = 0;
 
 
--- kernel/gdt_init.h
+++ kernel/gdt_init.h
@@ -12,6 +12,27 @@ struct gdt{
 
 __attribute__((section(".init_data"))) struct gdt gdt_ptr={0,0};
 
+#define CODE64_0 TYPE_CODE64 | DPL_0 | S | P | L
+#define DATA64_0 TYPE_DATA64 | DPL_0 | S | P
+#define CODE64_3 TYPE_CODE64 | DPL_3 | S | P | L
+#define DATA64_3 TYPE_DATA64 | DPL_3 | S | P
+#define CODE32_0 TYPE_CODE32 | DPL_0 | S | P | LIMIT32 | DB | G
+#define DATA32_0 TYPE_DATA32 | DPL_0 | S | P | LIMIT32 | DB | G
+
+#define TYPE_CODE64 0x8UL <<40
+#define TYPE_DATA64 0x2UL <<40
+#define TYPE_CODE32 0xAUL <<40
+#define TYPE_DATA32 0x2UL <<40
+#define TYPE_TSSGATE 0x9UL <<40
+#define TYPE_CALLGATE 0xCUL <<40
+#define S          1UL << 44
+#define DPL_0      0UL << 45
+#define DPL_3      3UL <<45
+#define P          1UL << 47
+#define LIMIT32    (0xFUL << 48) | 0xFFFF
+#define L          1UL << 53
+#define DB         1UL << 54
+#define G          1UL << 55
 
 
 #endif
