9437f56edf7d5f1fe1fd2b9a4b28129a025939ce
wind33333333 <wind33333333@gmail.com>
2024-06-11 20:58:25 +0800

    20240525

--- kernel/cpuinfo.c
+++ kernel/cpuinfo.c
@@ -9,7 +9,7 @@ void get_cpuinfo(unsigned int *p){
             "rdmsr               \n\t"
             :"=a"(*p)::"%rcx", "%rdx");
 
-    if(bsp_flags)
+    if(bsp_flags) {
         __asm__ __volatile__(
                 "mov $0, %%eax \n\t"
                 "cpuid         \n\t"
@@ -17,34 +17,36 @@ void get_cpuinfo(unsigned int *p){
                 "mov %%edx, 4(%%edi)\n\t"
                 "mov %%ecx, 8(%%edi) \n\t"
                 "movb $0, 12(%%edi) \n\t"
-                ::"D"(&cpu_info.manufacturer_name):"%rax","%rbx","%rcx","%rdx");
+                ::"D"(&cpu_info.manufacturer_name):"%rax", "%rbx", "%rcx", "%rdx");
 
-    BOCHS_DG();
-    __asm__ __volatile__(
-            "mov $0x80000002, %%eax \n\t"
-            "cpuid         \n\t"
-            "mov %%eax, (%%rdi)   \n\t"
-            "mov %%ebx, 4(%%rdi)  \n\t"
-            "mov %%ecx, 8(%%edi)  \n\t"
-            "mov %%edx, 12(%%edi) \n\t"
+        BOCHS_DG();
+        __asm__ __volatile__(
+                "mov $0x80000002, %%eax \n\t"
+                "cpuid         \n\t"
+                "mov %%eax, (%%rdi)   \n\t"
+                "mov %%ebx, 4(%%rdi)  \n\t"
+                "mov %%ecx, 8(%%edi)  \n\t"
+                "mov %%edx, 12(%%edi) \n\t"
 
-            "mov $0x80000003, %%eax \n\t"
-            "cpuid         \n\t"
-            "mov %%eax, 16(%%rdi)   \n\t"
-            "mov %%ebx, 20(%%rdi)  \n\t"
-            "mov %%ecx, 24(%%edi)  \n\t"
-            "mov %%edx, 28(%%edi) \n\t"
+                "mov $0x80000003, %%eax \n\t"
+                "cpuid         \n\t"
+                "mov %%eax, 16(%%rdi)   \n\t"
+                "mov %%ebx, 20(%%rdi)  \n\t"
+                "mov %%ecx, 24(%%edi)  \n\t"
+                "mov %%edx, 28(%%edi) \n\t"
 
-            "mov $0x80000004, %%eax \n\t"
-            "cpuid         \n\t"
-            "mov %%eax, 32(%%rdi)   \n\t"
-            "mov %%ebx, 36(%%rdi)  \n\t"
-            "mov %%ecx, 40(%%edi)  \n\t"
-            "mov %%edx, 44(%%edi) \n\t"
+                "mov $0x80000004, %%eax \n\t"
+                "cpuid         \n\t"
+                "mov %%eax, 32(%%rdi)   \n\t"
+                "mov %%ebx, 36(%%rdi)  \n\t"
+                "mov %%ecx, 40(%%edi)  \n\t"
+                "mov %%edx, 44(%%edi) \n\t"
 
-            ::"D"(&cpu_info.model_name):"%rax","%rbx","%rcx","%rdx");
+                "mov $0, 48(%%edi) \n\t"
+                ::"D"(&cpu_info.model_name):"%rax", "%rbx", "%rcx", "%rdx");
 
-    color_printk(YELLOW, BLACK, "%s \n",cpu_info.model_name);
+        color_printk(YELLOW, BLACK, "%s \n", cpu_info.model_name);
+    }
         return;
 }
 
