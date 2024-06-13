a188009d75ad10adf246b80c0d2a168ca7dcff57
wind33333333 <wind33333333@gmail.com>
2024-06-13 09:48:03 +0800

    20240525

--- kernel/acpi_init.c
+++ kernel/acpi_init.c
@@ -5,15 +5,14 @@
 
 __attribute__((section(".init_text"))) void acpi_init(void) {
     if (bsp_flags) {
+        RSDP *rsdp = (RSDP *) 0xe0000;
         RSDT *rsdt;
         MADT *madt;
         HPET *hpet;
 
-        for (RSDP *rsdp = (RSDP *) 0xe0000; rsdp < 0x100000; rsdp = (unsigned long *) rsdp + 2) {
+        for (; rsdp < 0x100000; rsdp = (unsigned long *) rsdp + 2) {
             if (*(unsigned long *) rsdp == 0x2052545020445352) {  //'RSD PTR '
                 rsdt = (RSDT *) rsdp->RsdtAddress;
-                color_printk(YELLOW, BLACK, "RSDP: %#018lX  RSDT: %#018lX  ", rsdp,
-                             rsdt);
                 break;
             }
         }
@@ -22,12 +21,10 @@ __attribute__((section(".init_text"))) void acpi_init(void) {
             switch (*(unsigned int *) rsdt->Entry[i]) {
                 case 0x43495041:        //"APIC"
                     madt = (MADT *) rsdt->Entry[i];
-                    color_printk(YELLOW, BLACK, "ACPI: %#018lX  ", madt);
                     break;
                 case 0x54455048:        //"HPET"
                     hpet = (HPET *) rsdt->Entry[i];
                     hpet_baseaddr = (unsigned int *) hpet->BaseAddressUpper;
-                    color_printk(YELLOW, BLACK, "HPET: %#018lX  \n", hpet);
                     break;
             }
         }
@@ -35,11 +32,14 @@ __attribute__((section(".init_text"))) void acpi_init(void) {
         for (unsigned int i = 0; i < (madt->Length - 44) / 8; i++) {
             if (madt->ioapic[i].type == 1) {
                 ioapic_baseaddr = (unsigned int *) madt->ioapic[i].ioapic_address;
-                color_printk(YELLOW, BLACK, "IOAPIC ADDR: %#018lX  HPET ADDR: %#018lX\n",
-                             ioapic_baseaddr, hpet_baseaddr);
             }
-
         }
+
+        color_printk(YELLOW, BLACK, "RSDP: %#018lX \tRSDT: %#018lX\n", rsdp,
+                     rsdt);
+        color_printk(YELLOW, BLACK, "APIC: %#018lX \tIOAPIC ADDR: %#018lX\n", madt,
+                     ioapic_baseaddr);
+        color_printk(YELLOW, BLACK, "HPET: %#018lX \tHPET ADDR: %#018lX\n", hpet, hpet_baseaddr);
     }
 
     return;
