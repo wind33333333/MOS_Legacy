9514a96a194ed716a716cda1963ef12bc52dd9a8
wind33333333 <wind33333333@gmail.com>
2024-06-17 18:28:25 +0800

    20240525

--- kernel/main.c
+++ kernel/main.c
@@ -27,12 +27,7 @@ __attribute__((section(".init_text"))) void Kernel_init(void) {
     ap_init(cpu_id);                             //初始化ap核
     papg_init();                                 //初始化内核页表
 
-
     sti();
-
-//    color_printk(YELLOW, BLACK, "Kernelstart: %#018lX Kernelend: %#018lX \n",memory_management_struct.kernel_start, memory_management_struct.kernel_end);
-//    __asm__ __volatile__ ("int $0 \n\t":: :);
-//    int i = 1 / 0;
 
     while (1);
 }
