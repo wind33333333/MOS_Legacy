32956c9d141b1dd2c8ce5f7501e8ae4bba940d59
wind33333333 <wind33333333@gmail.com>
2024-06-19 22:46:46 +0800

    20240525

--- kernel/linkage.h
+++ kernel/linkage.h
@@ -19,7 +19,6 @@
 #define ENTRY(name, handler, has_error_code) \
     .global name; \
 name:; \
-     xchg %bx,%bx;             \
     .if has_error_code; \
         pushq $0; \
     .endif; \
@@ -28,5 +27,29 @@ name:; \
     xchgq %rax, (%rsp); \
     jmp interrupt_entry;
 
+#define	R15		0x00
+#define	R14		0x08
+#define	R13		0x10
+#define	R12		0x18
+#define	R11		0x20
+#define	R10		0x28
+#define	R9		0x30
+#define	R8		0x38
+#define	RBX		0x40
+#define	RCX		0x48
+#define	RDX		0x50
+#define	RSI		0x58
+#define	RDI		0x60
+#define	RBP		0x68
+#define	DS		0x70
+#define	ES		0x78
+#define	RAX		0x80
+#define	FUNC		0x88
+#define	ERRCODE		0x90
+#define	RIP		0x98
+#define	CS		0xa0
+#define	RFLAGS		0xa8
+#define	OLDRSP		0xb0
+#define	OLDSS		0xb8
 
 #endif
