e1bc46663f9a1851cc13c458169ade949e74b4a3
wind33333333 <wind33333333@gmail.com>
2024-06-26 22:24:41 +0800

    20240525

--- kernel/papg.c
+++ kernel/papg.c
@@ -3,8 +3,21 @@
 __attribute__((section(".init_text"))) void papg_init(unsigned char bsp_flags) {
 
     if (bsp_flags) {
-        unsigned long papg_num = Virt_To_Phy(memory_management_struct.kernel_end) / 4096;
+        unsigned long papg_num = 0;
+        unsigned long papg_tablenum = 0;
+        Virt_To_Phy(memory_management_struct.kernel_end) / 4096;
         if (Virt_To_Phy(memory_management_struct.kernel_end) % 4096)
+            papg_num++;
+
+        for (unsigned int i = 0; i < 3; i++) {
+            papg_num = papg_num / 512;
+            papg_tablenum += papg_num;
+            if (papg_num % 512)
+                papg_tablenum++;
+        }
+
+        color_printk(ORANGE, BLACK, "OS PaPg: %ld  PaPgT: %ld\n",papg_num,papg_tablenum);
+
 
 
         color_printk(ORANGE, BLACK, "OS Can Used Total 4K PAGEs: %ld \tAlloc: %ld \tFree: %ld\n",
