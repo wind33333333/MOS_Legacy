1cf613c5011251034034ab04c1b382514636e1b5
wind33333333 <wind33333333@gmail.com>
2024-07-09 22:08:22 +0800

    20240525

--- kernel/papg.c
+++ kernel/papg.c
@@ -34,29 +34,29 @@ __attribute__((section(".init_text"))) void papg_init(unsigned char bsp_flags) {
         addr = (unsigned long) pml4e_pbaseaddr;
 
         for (unsigned int i = 0; i < pml4e_num; i++) {
-            pml4_bak[i] = pml4t_vbase[i];  //备份原PML4E
-            pml4t_vbase[i] = addr | 0x7;  //修改PML4E
+            pml4_bak[i] = upml4t_vbase[i];  //备份原PML4E
+            upml4t_vbase[i] = addr | 0x7;  //修改PML4E
             addr += 4096;
         }
 
         for (unsigned int i = 0; i < pdpte_num; i++) {
-            pdptt_vbase[i] = addr | 0x7;  //修改PDPTE
+            updptt_vbase[i] = addr | 0x7;  //修改PDPTE
             addr += 4096;
         }
 
         for (unsigned int i = 0; i < pde_num; i++) {
-            pdt_vbase[i] = addr | 0x3;  //修改PDE
+            updt_vbase[i] = addr | 0x3;  //修改PDE
             addr += 4096;
         }
 
         for (unsigned long i = 0; i < pte_num; i++) {
-            ptt_vbase[i] = i * 0x1000 | 0x83;  //修改PTE
+            uptt_vbase[i] = i * 0x1000 | 0x83;  //修改PTE
         }
 
         for (unsigned int i = 0; i < pml4e_num; i++) {
-            //   __PML4T[i] = pml4t_vbase[i];            //修改正式内核PML4T 低
-            __PML4T[i + 256] = pml4t_vbase[i];        //修改正式内核PML4T 高
-            pml4t_vbase[i] = pml4_bak[i];           //还原PML4E
+            //   __PML4T[i] = upml4t_vbase[i];            //修改正式内核PML4T 低
+            __PML4T[i + 256] = upml4t_vbase[i];        //修改正式内核PML4T 高
+            upml4t_vbase[i] = pml4_bak[i];           //还原PML4E
         }
 
 
@@ -74,7 +74,7 @@ __attribute__((section(".init_text"))) void papg_init(unsigned char bsp_flags) {
                 "mov    %%rax,%%cr3 \n\t"
                 ::"a"(addr):);
 
-        phy_to_virt(Pos.FB_addr, Pos.FB_length);
+        kphy_to_virt(Pos.FB_addr, Pos.FB_length);
 
     }
 
@@ -86,76 +86,57 @@ __attribute__((section(".init_text"))) void papg_init(unsigned char bsp_flags) {
     return;
 }
 
-void phy_to_virt(unsigned long phy_addr, unsigned long phy_len) {
+void kphy_to_virt(unsigned long phy_addr, unsigned long phy_len) {
 
     unsigned long y;
     unsigned long addr = Virt_To_Phy(phy_addr);
-
-/*    y = phy_len / (4096UL * 512 * 512 * 512);
-    if (phy_len % (4096UL * 512 * 512 * 512))
-        y++;
-    for (unsigned long i = 0; i < y; i++) {
-        if (pml4t_vbase[(addr >> 39) + i] == 0)
-            pml4t_vbase[(addr >> 39) + i] = (unsigned long) alloc_pages(1) | 0x7;
-    }
-
-    y = phy_len / (4096UL * 512 * 512);
-    if (phy_len % (4096UL * 512 * 512))
-        y++;
-    for (unsigned long i = 0; i < y; i++) {
-        if (pdptt_vbase[(addr >> 30) + i] == 0)
-            pdptt_vbase[(addr >> 30) + i] = (unsigned long) alloc_pages(1) | 0x7;
-    }
-
-    y = phy_len / (4096UL * 512);
-    if (phy_len % (4096UL * 512))
-        y++;
-    for (unsigned long i = 0; i < y; i++) {
-        if (pdt_vbase[(addr >> 21) + i] == 0)
-            pdt_vbase[(addr >> 21) + i] = (unsigned long) alloc_pages(1) | 0x3;
-    }
-
-    y = phy_len / 4096;
-    if (phy_len % 4096)
-        y++;
-    for (unsigned long i = 0; i < y; i++) {
-        if (ptt_vbase[(addr >> 12) + i] == 0)
-            ptt_vbase[(addr >> 12) + i] = addr + i * 4096 | 0x83;
-    }*/
 
     y = phy_len / (4096UL * 512 * 512 * 512);
     if (phy_len % (4096UL * 512 * 512 * 512))
         y++;
     for (unsigned long i = 0; i < y; i++) {
-        if (pml4t_vbase[(addr >> 39) + 256 + i] == 0)
-            pml4t_vbase[(addr >> 39) + 256 + i] = (unsigned long) alloc_pages(1) | 0x7;
+        if (kpml4t_vbase[(addr >> 39) + i] == 0) {
+            kpml4t_vbase[(addr >> 39) + i] = (unsigned long) alloc_pages(1) | 0x7;
+//            for (unsigned long j = 0; j < 512; j++) {
+//                kpdptt_vbase[(addr >> 30) + i * 512 + j] = 0x0UL;
+//            }
+        }
     }
 
     y = phy_len / (4096UL * 512 * 512);
     if (phy_len % (4096UL * 512 * 512))
         y++;
     for (unsigned long i = 0; i < y; i++) {
-        if (pdptt_vbase[(addr >> 30) + 0x20000 + i] == 0)
-            pdptt_vbase[(addr >> 30) + 0x20000 + i] = (unsigned long) alloc_pages(1) | 0x7;
+        if (kpdptt_vbase[(addr >> 30) + i] == 0) {
+            kpdptt_vbase[(addr >> 30) + i] = (unsigned long) alloc_pages(1) | 0x7;
+//            for (unsigned long j = 0; j < 512; j++) {
+//                kpdt_vbase[((addr >> 21) & 0x1FF) + i * 512 + j] = 0x0UL;
+//            }
+        }
     }
 
     y = phy_len / (4096UL * 512);
     if (phy_len % (4096UL * 512))
         y++;
     for (unsigned long i = 0; i < y; i++) {
-        if (pdt_vbase[(addr >> 21) + 0x4000000 + i] == 0)
-            pdt_vbase[(addr >> 21) + 0x4000000 + i] = (unsigned long) alloc_pages(1) | 0x3;
+
+        if (kpdt_vbase[(addr >> 21) + i] == 0) {
+            kpdt_vbase[(addr >> 21) + i] = (unsigned long) alloc_pages(1) | 0x3;
+//            for (unsigned long j = 0; j < 512; j++) {
+//                kptt_vbase[(addr >> 12) + i * 512 + j] = 0x0UL;
+//            }
+        }
     }
 
     y = phy_len / 4096;
     if (phy_len % 4096)
         y++;
     for (unsigned long i = 0; i < y; i++) {
-        if (ptt_vbase[(addr >> 12) + 0x800000000 + i] == 0)
-            ptt_vbase[(addr >> 12) + 0x800000000 + i] = addr + i * 4096 | 0x83;
+
+        if (kptt_vbase[(addr >> 12) + i] == 0)
+            kptt_vbase[(addr >> 12) + i] = addr + i * 4096 | 0x83;
     }
-
-
 
     return;
 }
+
--- kernel/papg.h
+++ kernel/papg.h
@@ -6,12 +6,18 @@
 #include "cpuinfo.h"
 
 void papg_init(unsigned char bsp_flags);
-void phy_to_virt(unsigned long phy_addr, unsigned long phy_len);
+void kphy_to_virt(unsigned long phy_addr, unsigned long phy_len);
+void uphy_to_virt(unsigned long phy_addr, unsigned long phy_len);
 
-unsigned long* pml4t_vbase = (unsigned long*)0xFFFFFFFFFFFFF000;  //pml4虚拟地址基址
-unsigned long* pdptt_vbase = (unsigned long*)0xFFFFFFFFFFE00000;  //pdpt虚拟地址基址
-unsigned long* pdt_vbase = (unsigned long*)0xFFFFFFFFC0000000;    //pd虚拟地址基址
-unsigned long* ptt_vbase = (unsigned long*)0xFFFFFF8000000000;    //pt虚拟地址基址
+unsigned long* upml4t_vbase = (unsigned long*)0xFFFFFFFFFFFFF000;  //用户pml4虚拟地址基址
+unsigned long* updptt_vbase = (unsigned long*)0xFFFFFFFFFFE00000;  //用户pdpt虚拟地址基址
+unsigned long* updt_vbase = (unsigned long*)0xFFFFFFFFC0000000;    //用户pd虚拟地址基址
+unsigned long* uptt_vbase = (unsigned long*)0xFFFFFF8000000000;    //用户pt虚拟地址基址
+
+unsigned long* kpml4t_vbase = (unsigned long*)0xFFFFFFFFFFFFF800;  //内核pml4虚拟地址基址
+unsigned long* kpdptt_vbase = (unsigned long*)0xFFFFFFFFFFF00000;  //内核pdpt虚拟地址基址
+unsigned long* kpdt_vbase = (unsigned long*)0xFFFFFFFFE0000000;    //内核pd虚拟地址基址
+unsigned long* kptt_vbase = (unsigned long*)0xFFFFFFC000000000;    //内核pt虚拟地址基址
 
 extern unsigned long __PML4T[512];
 
