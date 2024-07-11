20cf5b7348095cd567cc8fa2caf89a8e7c1a53da
wind33333333 <wind33333333@gmail.com>
2024-07-11 19:13:15 +0800

    20240525

--- kernel/papg.c
+++ kernel/papg.c
@@ -23,67 +23,6 @@ __attribute__((section(".init_text"))) void papg_init(unsigned char bsp_flags) {
             upml4t_vbase[i] = pml4_bak[i];           //还原PML4E
         }
 
-/*
-
-        unsigned long pml4e_num = 0;
-        unsigned long pdpte_num = 0;
-        unsigned long pde_num = 0;
-        unsigned long pte_num = 0;
-        unsigned long pml4e_pbaseaddr = 0;
-        unsigned long pml4_bak[256] = {0};
-
-        pte_num = Virt_To_Phy(memory_management_struct.kernel_end) / 4096;     //计算内核总pte数量
-        if (Virt_To_Phy(memory_management_struct.kernel_end) % 4096)
-            pte_num++;
-
-        pde_num = pte_num / 512;                //计算内核pde数量
-        if (pte_num % 512)
-            pde_num++;
-
-        pdpte_num = pde_num / 512;              //计算内核pdpte数量
-        if (pde_num % 512)
-            pdpte_num++;
-
-        pml4e_num = pdpte_num / 512;             //计算内核pml4e数量
-        if (pdpte_num % 512)
-            pml4e_num++;
-
-        pml4e_pbaseaddr = (unsigned long) alloc_pages(
-                pml4e_num + pdpte_num + pde_num);   //申请内核pml4e、pdpte、pde、pte页表空间
-
-        addr = (unsigned long) pml4e_pbaseaddr;
-
-        for (unsigned int i = 0; i < pml4e_num; i++) {
-            pml4_bak[i] = upml4t_vbase[i];  //备份原PML4E
-            upml4t_vbase[i] = addr | 0x7;  //修改PML4E
-            addr += 4096;
-        }
-
-        for (unsigned int i = 0; i < pdpte_num; i++) {
-            updptt_vbase[i] = addr | 0x7;  //修改PDPTE
-            addr += 4096;
-        }
-
-        for (unsigned int i = 0; i < pde_num; i++) {
-            updt_vbase[i] = addr | 0x3;  //修改PDE
-            addr += 4096;
-        }
-
-        for (unsigned long i = 0; i < pte_num; i++) {
-            uptt_vbase[i] = i * 0x1000 | 0x83;  //修改PTE
-        }
-
-        for (unsigned int i = 0; i < pml4e_num; i++) {
-            //   __PML4T[i] = upml4t_vbase[i];            //修改正式内核PML4T 低
-            __PML4T[i + 256] = upml4t_vbase[i];        //修改正式内核PML4T 高
-            upml4t_vbase[i] = pml4_bak[i];           //还原PML4E
-        }
-*/
-
-
-/*        color_printk(ORANGE, BLACK,
-                     "PML4E: %ld  PDPTE: %ld  PDE: %ld  PTE: %ld  pml4e_pbase: %#018lX\n",
-                     pml4e_num, pdpte_num, pde_num, pte_num, pml4e_pbaseaddr);*/
         color_printk(ORANGE, BLACK, "OS Can Used Total 4K PAGEs: %ld \tAlloc: %ld \tFree: %ld\n",
                      memory_management_struct.total_pages, memory_management_struct.alloc_pages,
                      memory_management_struct.free_pages);
