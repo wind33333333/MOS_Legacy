9d3d5f97ac9dba03d4e97168674c7be492aa3a1f
wind33333333 <wind33333333@gmail.com>
2024-07-16 14:15:33 +0800

    20240525

--- kernel/papg.h
+++ kernel/papg.h
@@ -8,7 +8,7 @@
 
 void papg_init(unsigned char bsp_flags);
 void map_pages(unsigned long paddr, unsigned long vaddr,unsigned long page_num, unsigned long attr);
-void unmap_pages(unsigned long addr, unsigned long len);
+void unmap_pages(unsigned long paddr, unsigned long page_num);
 
 unsigned long* pml4t_vbase = (unsigned long*)0xFFFFFFFFFFFFF000;  //pml4虚拟地址基址
 unsigned long* pdptt_vbase = (unsigned long*)0xFFFFFFFFFFE00000;  //pdpt虚拟地址基址
