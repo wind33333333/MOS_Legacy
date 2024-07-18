6ec95901cd7cf7e59e873cb2ad7fd2a5592673bc
wind33333333 <wind33333333@gmail.com>
2024-07-18 15:59:26 +0800

    20240525

--- kernel/main.c
+++ kernel/main.c
@@ -31,6 +31,7 @@ __attribute__((section(".init_text"))) void Kernel_init(void) {
     //ENABLE_HPET_TIMES(*hpetRegisters.TIM0_CONF,*hpetRegisters.TIM0_COMP,0x3000000,HPET_PERIODIC,0);
     //enable_apic_time(0xF000,APIC_TSC_DEADLINE,0x20);
 
+
     sti();
     while (1);
 }
